63fd9ece34af66a5fa111c8286602489693cc305
Malikslrb <maliksilarbi001@gmail.com>
2025-05-24 00:05:07 +0100

    setupspringboot

---
+++ backend/src/main/java/com/PPlite/pplite_backend/PpliteBackendApplication.java
@@ -0,0 +1,13 @@
+package com.PPlite.pplite_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpliteBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PpliteBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/PPlite/pplite_backend/PpliteBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PPlite.pplite_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpliteBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
